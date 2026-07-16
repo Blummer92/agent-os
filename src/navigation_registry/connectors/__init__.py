59b8199feb5532a485c151100732c9a181b6a292
Zachary Blumstein <32861845+Blummer92@users.noreply.github.com>
2026-07-16 00:06:06 -0400

    Export Notion contract adapter

--- src/navigation_registry/connectors/__init__.py
+++ src/navigation_registry/connectors/__init__.py
@@ -2,10 +2,18 @@
 
 from .base import ConnectorError, ConnectorErrorCode, RegistryResource
 from .notion import NotionReadOnlyConnector
+from .notion_contract_adapter import (
+    CACHED_NOTION_INDEX_SOURCE,
+    LIVE_NOTION_SOURCE,
+    NotionContractAdapter,
+)
 
 __all__ = [
+    "CACHED_NOTION_INDEX_SOURCE",
     "ConnectorError",
     "ConnectorErrorCode",
+    "LIVE_NOTION_SOURCE",
+    "NotionContractAdapter",
     "NotionReadOnlyConnector",
     "RegistryResource",
 ]
