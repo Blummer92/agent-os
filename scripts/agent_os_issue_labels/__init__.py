4af9a6eb0b3f9bdd3891c655c3f516c4c8ae1811
Zachary Blumstein <32861845+Blummer92@users.noreply.github.com>
2026-07-18 16:35:42 -0400

    L5A export issue label planner

--- scripts/agent_os_issue_labels/__init__.py
+++ scripts/agent_os_issue_labels/__init__.py
@@ -1,5 +1,6 @@
-"""Offline Agent OS issue label checker."""
+"""Offline Agent OS issue label checker and application planner."""
 
 from .checker import evaluate_issue_labels
+from .planner import LabelApplicationPlan, plan_label_application
 
-__all__ = ["evaluate_issue_labels"]
+__all__ = ["LabelApplicationPlan", "evaluate_issue_labels", "plan_label_application"]
