cbf11ce2e746cad8a3fbf2ac9a5980b03789ae64
Zachary Blumstein <32861845+Blummer92@users.noreply.github.com>
2026-08-19 22:27:31 -0400

    Export bounded high-reasoning proposal contract for #1257

--- scripts/agent_os_pr_remediation/__init__.py
+++ scripts/agent_os_pr_remediation/__init__.py
@@ -27,6 +27,17 @@ from .coordination import (
     serialize_resolution_plan,
 )
 from .evidence_assembly import EvidenceAssemblyResult, GitHubEvidenceReader, assemble_prr_evidence
+from .high_reasoning_proposal import (
+    FrozenProposalPlan,
+    ProposalOutcome,
+    ProposalProviderUnavailable,
+    ProposalRequest,
+    ProposalResult,
+    ProposalType,
+    evaluate_plan_currentness,
+    propose_high_reasoning_plan,
+    serialize_frozen_proposal,
+)
 from .models import EvidenceValidationError, NormalizedPRSnapshot, NormalizedReviewThread, canonical_json, deterministic_id
 from .normalization import classify_review_thread_payload, normalize_pr_snapshot, normalize_review_thread, normalize_review_threads
 from .planning import COMPUTE_ROUTES, FINDING_CLASSIFICATIONS, FindingCandidate, PlannedFinding, RemediationPlan, RemediationTask, plan_remediation
@@ -36,15 +47,18 @@ __all__ = [
     "CIEvidenceIdentity", "CIEvidenceRecoveryPlan", "COMPUTE_ROUTES",
     "DEFAULT_DIAGNOSTIC_EXCERPT_LINES", "DIAGNOSTIC_EXCERPT_EXPANSION_LINES",
     "EvidenceAssemblyResult", "EvidenceValidationError", "FINDING_CLASSIFICATIONS",
-    "FindingCandidate", "FindingFixEvidence", "GitHubEvidenceReader",
+    "FindingCandidate", "FindingFixEvidence", "FrozenProposalPlan", "GitHubEvidenceReader",
     "MAX_DIAGNOSTIC_EXCERPT_LINES", "MIN_DIAGNOSTIC_EXCERPT_LINES",
     "NormalizedPRSnapshot", "NormalizedReviewThread", "PlannedFinding", "PreflightResult",
-    "RECOVERY_FAILURE_REASONS", "RECOVERY_PATHS", "RecoveryObservation", "RemediationPlan",
-    "RemediationTask", "ResolutionPlan", "SUGGESTED_ACTIONS", "ThreadResolutionEvidence",
-    "VALIDATION_STATES", "ValidationCategoryResult", "ValidationEvidenceBinding",
-    "assemble_prr_evidence", "canonical_json", "classify_review_thread_payload",
-    "coordinate_resolution", "deterministic_id", "diagnostic_excerpt_lines",
-    "expand_diagnostic_excerpt_lines", "normalize_pr_snapshot", "normalize_review_thread",
-    "normalize_review_threads", "plan_ci_evidence_recovery", "plan_remediation", "preflight",
-    "resolution_plan_id", "serialize_resolution_plan",
+    "ProposalOutcome", "ProposalProviderUnavailable", "ProposalRequest", "ProposalResult",
+    "ProposalType", "RECOVERY_FAILURE_REASONS", "RECOVERY_PATHS", "RecoveryObservation",
+    "RemediationPlan", "RemediationTask", "ResolutionPlan", "SUGGESTED_ACTIONS",
+    "ThreadResolutionEvidence", "VALIDATION_STATES", "ValidationCategoryResult",
+    "ValidationEvidenceBinding", "assemble_prr_evidence", "canonical_json",
+    "classify_review_thread_payload", "coordinate_resolution", "deterministic_id",
+    "diagnostic_excerpt_lines", "evaluate_plan_currentness", "expand_diagnostic_excerpt_lines",
+    "normalize_pr_snapshot", "normalize_review_thread", "normalize_review_threads",
+    "plan_ci_evidence_recovery", "plan_remediation", "preflight",
+    "propose_high_reasoning_plan", "resolution_plan_id", "serialize_frozen_proposal",
+    "serialize_resolution_plan",
 ]
