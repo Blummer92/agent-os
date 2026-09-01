0eeb58fac482a1c50d9a4e70be45155455ca6f77
Zachary Blumstein <32861845+Blummer92@users.noreply.github.com>
2026-08-31 22:47:30 -0400

    CRH5 export review attack plan contract

--- scripts/agent_os_pr_remediation/__init__.py
+++ scripts/agent_os_pr_remediation/__init__.py
@@ -1,47 +1,27 @@
 """Pure-local PR review remediation normalization, preflight, planning, and coordination."""
 
 from .ci_evidence_recovery import (
-    DEFAULT_DIAGNOSTIC_EXCERPT_LINES,
-    DIAGNOSTIC_EXCERPT_EXPANSION_LINES,
-    MAX_DIAGNOSTIC_EXCERPT_LINES,
-    MIN_DIAGNOSTIC_EXCERPT_LINES,
-    RECOVERY_FAILURE_REASONS,
-    RECOVERY_PATHS,
-    CIEvidenceIdentity,
-    CIEvidenceRecoveryPlan,
-    RecoveryObservation,
-    diagnostic_excerpt_lines,
-    expand_diagnostic_excerpt_lines,
-    plan_ci_evidence_recovery,
+    DEFAULT_DIAGNOSTIC_EXCERPT_LINES, DIAGNOSTIC_EXCERPT_EXPANSION_LINES,
+    MAX_DIAGNOSTIC_EXCERPT_LINES, MIN_DIAGNOSTIC_EXCERPT_LINES,
+    RECOVERY_FAILURE_REASONS, RECOVERY_PATHS, CIEvidenceIdentity,
+    CIEvidenceRecoveryPlan, RecoveryObservation, diagnostic_excerpt_lines,
+    expand_diagnostic_excerpt_lines, plan_ci_evidence_recovery,
 )
 from .coordination import (
-    SUGGESTED_ACTIONS,
-    VALIDATION_STATES,
-    FindingFixEvidence,
-    ResolutionPlan,
-    ThreadResolutionEvidence,
-    ValidationCategoryResult,
-    ValidationEvidenceBinding,
-    coordinate_resolution,
-    resolution_plan_id,
-    serialize_resolution_plan,
+    SUGGESTED_ACTIONS, VALIDATION_STATES, FindingFixEvidence, ResolutionPlan,
+    ThreadResolutionEvidence, ValidationCategoryResult, ValidationEvidenceBinding,
+    coordinate_resolution, resolution_plan_id, serialize_resolution_plan,
 )
 from .evidence_assembly import EvidenceAssemblyResult, GitHubEvidenceReader, assemble_prr_evidence
 from .models import EvidenceValidationError, NormalizedPRSnapshot, NormalizedReviewThread, canonical_json, deterministic_id
 from .normalization import classify_review_thread_payload, normalize_pr_snapshot, normalize_review_thread, normalize_review_threads
 from .planning import COMPUTE_ROUTES, FINDING_CLASSIFICATIONS, FindingCandidate, PlannedFinding, RemediationPlan, RemediationTask, plan_remediation
 from .preflight import PreflightResult, preflight
+from .review_attack_plan import RequiredAttack, ReviewAttackPlan, build_review_attack_plan
 from .review_evidence import (
-    ADVERSARIAL_RISKS,
-    FULL_REVIEW_INVALIDATORS,
-    NO_AI_CHANGE_KINDS,
-    ReviewDepth,
-    ReviewDepthDecision,
-    ReviewEvidencePacket,
-    ReviewRiskEvidence,
-    build_review_evidence_packet,
-    review_invalidation_scope,
-    select_review_depth,
+    ADVERSARIAL_RISKS, FULL_REVIEW_INVALIDATORS, NO_AI_CHANGE_KINDS, ReviewDepth,
+    ReviewDepthDecision, ReviewEvidencePacket, ReviewRiskEvidence,
+    build_review_evidence_packet, review_invalidation_scope, select_review_depth,
 )
 
 __all__ = [
@@ -52,9 +32,10 @@ __all__ = [
     "MAX_DIAGNOSTIC_EXCERPT_LINES", "MIN_DIAGNOSTIC_EXCERPT_LINES", "NO_AI_CHANGE_KINDS",
     "NormalizedPRSnapshot", "NormalizedReviewThread", "PlannedFinding", "PreflightResult",
     "RECOVERY_FAILURE_REASONS", "RECOVERY_PATHS", "RecoveryObservation", "RemediationPlan",
-    "RemediationTask", "ResolutionPlan", "ReviewDepth", "ReviewDepthDecision", "ReviewEvidencePacket",
-    "ReviewRiskEvidence", "SUGGESTED_ACTIONS", "ThreadResolutionEvidence", "VALIDATION_STATES",
-    "ValidationCategoryResult", "ValidationEvidenceBinding", "assemble_prr_evidence",
+    "RemediationTask", "RequiredAttack", "ResolutionPlan", "ReviewAttackPlan", "ReviewDepth",
+    "ReviewDepthDecision", "ReviewEvidencePacket", "ReviewRiskEvidence", "SUGGESTED_ACTIONS",
+    "ThreadResolutionEvidence", "VALIDATION_STATES", "ValidationCategoryResult",
+    "ValidationEvidenceBinding", "assemble_prr_evidence", "build_review_attack_plan",
     "build_review_evidence_packet", "canonical_json", "classify_review_thread_payload",
     "coordinate_resolution", "deterministic_id", "diagnostic_excerpt_lines",
     "expand_diagnostic_excerpt_lines", "normalize_pr_snapshot", "normalize_review_thread",
