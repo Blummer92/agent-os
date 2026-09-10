8a114bf573c6debef1be2b5f551088f54a1082b0
Zachary Blumstein <32861845+Blummer92@users.noreply.github.com>
2026-09-10 09:45:03 -0400

    Expand SHA reconciliation regression matrix (#2274)

--- tests/test_sha_validator_reconciliation.py
+++ tests/test_sha_validator_reconciliation.py
@@ -3,13 +3,17 @@
 import pytest
 
 from scripts.agent_os_issue_labels.pr_branch_refresh_actions_runner import _sha40 as refresh_sha40
+from scripts.agent_os_pr_remediation.aggregate_failure_provenance import _sha as aggregate_sha
 from scripts.agent_os_pr_remediation.ci_evidence_recovery import _sha40 as recovery_sha40
 from scripts.agent_os_pr_remediation.coordination import _sha40 as coordination_sha40
+from scripts.agent_os_pr_remediation.normalization import _sha as normalization_sha
+from scripts.agent_os_pr_remediation.preflight import _validate_sha as preflight_sha
 from scripts.agent_os_pr_remediation.models import EvidenceValidationError
 
 LOWER = "a" * 40
 UPPER = "A" * 40
 MIXED = "aA" * 20
+INVALID_VALUES = (UPPER, MIXED, "a" * 39, "a" * 41, "g" * 40, "", None, 123, b"a" * 40)
 
 
 @pytest.mark.parametrize(
@@ -18,22 +22,13 @@ MIXED = "aA" * 20
         (recovery_sha40, EvidenceValidationError),
         (coordination_sha40, EvidenceValidationError),
         (refresh_sha40, ValueError),
+        (aggregate_sha, EvidenceValidationError),
+        (normalization_sha, EvidenceValidationError),
+        (preflight_sha, EvidenceValidationError),
     ],
 )
 def test_reconciled_sha_validators_preserve_lowercase_and_reject_noncanonical_inputs(validator, error_type):
     assert validator(LOWER, "sha") == LOWER
-
-    invalid_values = (
-        UPPER,
-        MIXED,
-        "a" * 39,
-        "a" * 41,
-        "g" * 40,
-        "",
-        None,
-        123,
-        b"a" * 40,
-    )
-    for value in invalid_values:
+    for value in INVALID_VALUES:
         with pytest.raises(error_type):
             validator(value, "sha")
