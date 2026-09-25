e7fc2f9511d97ae0857268001e5efd6090a9d616
Zachary Blumstein <32861845+Blummer92@users.noreply.github.com>
2026-09-24 21:58:10 -0400

    Retire materialization architecture registration for #2926

--- tests/agent_os_issue_acceptance/test_architecture_boundaries.py
+++ tests/agent_os_issue_acceptance/test_architecture_boundaries.py
@@ -70,7 +70,6 @@ DOMAIN_RULES: tuple[tuple[str, frozenset[str], tuple[str, ...]], ...] = (
                 "merge_authorization",
                 "merge_authorization_source",
                 "planning_binding",
-                "draft_pr_materialization_admission",
                 "primary_pr_creation_admission",
             }
         ),
